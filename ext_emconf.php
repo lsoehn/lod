433f3a1e10b21f56d1a4c71792213480519a4030
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2025-03-07 17:07:50 +0000

    refactor: update emconf file according to TYPO3 Rector

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,17 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Linked Open Data for TYPO3',
     'description' => 'Provides a semantic layer for a TYPO3 with LOD API, terminology service, RDF serializer and IRI resolver',
     'author' => 'Torsten Schrade',
     'author_email' => 'Torsten.Schrade@adwmainz.de',
     'author_company' => 'Academy of Sciences and Literature | Mainz',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
     'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => '0',
-    'createDirs' => '',
     'version' => '1.0.0',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '12.0.0-12.4.99'
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-);
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
