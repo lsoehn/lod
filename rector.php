effac5533d5a32c91e431c57d5d98fd7891901c1
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2025-03-12 15:47:22 +0000

    test: add TYPO3 Rector configuration

---
+++ rector.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\PostRector\Rector\NameImportingPostRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\ValueObject\PhpVersion;
+use Ssch\TYPO3Rector\CodeQuality\General\ConvertImplicitVariablesToExplicitGlobalsRector;
+use Ssch\TYPO3Rector\CodeQuality\General\ExtEmConfRector;
+use Ssch\TYPO3Rector\Configuration\Typo3Option;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
+use Ssch\TYPO3Rector\Set\Typo3SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withPhpVersion(PhpVersion::PHP_83)
+    ->withSets([
+        Typo3SetList::CODE_QUALITY,
+        Typo3SetList::GENERAL,
+        Typo3LevelSetList::UP_TO_TYPO3_12,
+        DoctrineSetList::DOCTRINE_DBAL_40,
+    ])
+    // To have a better analysis from PHPStan, we teach it here some more things
+    ->withPHPStanConfigs([Typo3Option::PHPSTAN_FOR_RECTOR_PATH])
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        ConvertImplicitVariablesToExplicitGlobalsRector::class,
+    ])
+    ->withConfiguredRule(ExtEmConfRector::class, [
+        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.1.0-8.2.99',
+        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '12.4.0-12.4.99',
+        ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
+    ])
+    // If you use withImportNames(), you should consider excluding some TYPO3 files.
+    ->withSkip([
+        NameImportingPostRector::class => [
+            'ClassAliasMap.php',
+        ]
+    ])
+;
