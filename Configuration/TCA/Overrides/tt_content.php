b5ffb43ddb3f25b667e907b3f82e08af9a78c719
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2025-03-12 16:34:31 +0000

    chore: migrate plugin registration (should've been in last commit already)

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,30 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+defined('TYPO3') or die();
+
+ExtensionUtility::registerPlugin(
+    'Lod',
+    'Vocabulary',
+    'LOD: Vocabulary'
+);
+
+ExtensionUtility::registerPlugin(
+    'Lod',
+    'Api',
+    'LOD: Api'
+);
+
+ExtensionUtility::registerPlugin(
+    'Lod',
+    'Serializer',
+    'LOD: Serializer'
+);
+
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['lod_vocabulary'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('lod_vocabulary', 'FILE:EXT:lod/Configuration/FlexForms/VocabularyPlugin.xml');
+ExtensionManagementUtility::addPiFlexFormValue('lod_vocabulary', 'FILE:EXT:lod/Configuration/FlexForms/VocabularyPlugin.xml');
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['lod_serializer'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('lod_serializer', 'FILE:EXT:lod/Configuration/FlexForms/SerializerPlugin.xml');
+ExtensionManagementUtility::addPiFlexFormValue('lod_serializer', 'FILE:EXT:lod/Configuration/FlexForms/SerializerPlugin.xml');
